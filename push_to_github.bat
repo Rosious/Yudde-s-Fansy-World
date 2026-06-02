2bf7287ff546b1f847b1ca5b92527407922caa9b
Rosious <1332665526@qq.com>
2026-06-02 13:55:56 +0800

    chore: remove token file

--- push_to_github.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d D:\Yudde-Demo
-echo [%date% %time%] Attempting git push...
-git push https://rosious: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@github.com/Rosious/Yudde-s-Fansy-World.git master 2>&1
-if %ERRORLEVEL% == 0 (
-    echo [SUCCESS] Pushed to GitHub!
-) else (
-    echo [FAIL] Push failed - check network
-)
